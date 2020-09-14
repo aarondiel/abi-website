5635569a322a9b9d463c8720bb971ba82e234948
aarondiel <aaron.diel@t-online.de>
2020-09-14 19:35:22 +0200

    added DashbordItem

---
+++ src/components/DashbordItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="dashbord-item-wrapper">
+      <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashbordItem',
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style scoped>
+.dashbord-item-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #38383d;
+  border-radius: 0.4rem;
+}
+
+.dashbord-item-wrapper > h1 {
+  font-size: 2em;
+  font-weight: 500;
+  font-family: 'Roboto Mono';
+}
+</style>
