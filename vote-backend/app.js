07570c2c2a794f95d52a0eb0da9b2f3710d2b2e7
Vincent Schmandt <vincentscode@gmail.com>
2020-09-29 21:55:52 +0200

    cors.

--- vote-backend/app.js
+++ vote-backend/app.js
@@ -12,6 +12,7 @@ const frontend = require('./endpoints/frontend/routes');
 const app = express();
 
 // middleware
+app.use(cors())
 app.use(logger("dev"));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
