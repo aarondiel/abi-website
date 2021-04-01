22273ae9815aa9c834dd3692fd0d55c329d849ff
aarondiel <aaron.diel@t-online.de>
2021-04-01 13:53:17 +0200

    updated quotes validation

--- backend/models/quotes.js
+++ backend/models/quotes.js
@@ -4,36 +4,50 @@ const messageSchema = new mongoose.Schema({
 	type: {
 		type: String,
 
-		validate: (v) => {
-			return ['message', 'info'].indexOf(v) !== -1;
-		},
+		validate: [
+			(v) => {
+				return ['message', 'info'].indexOf(v) !== -1;
+			},
+			'not a valid message type'
+		],
 
-		required: true
+		required: [ true, 'no message type specified' ]
 	},
 
 	side: {
 		type: String,
 
-		validate: (v) => {
-			return ['left', 'right'].indexOf(v) !== -1;
-		},
+		validate: [
+			(v) => {
+				return ['left', 'right'].indexOf(v) !== -1;
+			},
+			'not a valid side'
+		],
 
-		required: function() {
-			// only required if message kind is 'message'
-			return this.kind === 'message';
-		}
+		required: [
+			function() {
+				// only required if message type is 'message'
+				return this.type === 'message';
+			},
+			'side was not specified'
+		]
 	},
 
 	name: {
 		type: String,
-		required: function() {
-			return this.kind === 'message';
-		}
+
+		required: [ function() {
+				// only required if message type is 'message'
+				console.log(this.type);
+				return this.type === 'message';
+			},
+			'name was not specified'
+		]
 	},
 
 	text: {
 		type: String,
-		required: true
+		required: [ true, 'no text specefied' ]
 	}
 });
 
