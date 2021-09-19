2dfda19360666062115415c8e7172082aaace8f3
aarondiel <aaron.diel@t-online.de>
2021-09-19 17:53:19 +0200

    extended query functionality + types

--- backend/src/queries.ts
+++ backend/src/queries.ts
@@ -1,7 +1,21 @@
-import { Pool } from 'pg'
+import pg from 'pg'
 import config from './config'
 
-const pool = new Pool({
+interface User {
+	[key: string]: any,
+	code: string,
+	firstname: string,
+	middlename?: string,
+	lastname: string,
+	email?: string,
+	gbrsigned: boolean
+}
+
+type Optional<Type> = {
+	[Property in keyof Type]?: Type[Property]
+}
+
+const pool = new pg.Pool({
 	user: config.postgresql.user,
 	password: config.postgresql.password,
 	host: config.postgresql.host,
@@ -9,31 +23,49 @@ const pool = new Pool({
 	port: config.postgresql.port
 })
 
-async function get_all_users() {
-	const users = await pool.query('SELECT * FROM users ORDER BY firstname ASC').rows
-	return users
+export async function get_all_users() {
+	const users = await pool.query('SELECT * FROM users')
+	return users.rows
 }
 
-async function add_new_user(code) {
-	const firstname = 'aaron'
-	const lastname = 'diel'
-	const email = 'aaron.diel@t-online.de'
-
+export async function add_new_user(user: User) {
 	const response = await pool.query(
-		'INSERT INTO users (code, firstname, lastname, email) VALUES ($1, $2, $3, $4)',
-		[ code, firstname, lastname, email ]
+		'INSERT INTO users (code, firstname, middlename, lastname, email, gbrsigned) VALUES ($1, $2, $3, $4, $5, $6)',
+		[ user.code, user.firstname, user.middlename, user.lastname, user.email, user.gbrsigned ]
 	)
 
 	return response
 }
 
-async function update_gbrsigned(code: string) {
-	const response = await pool.query(
-		'UPDATE users SET gbrsigned = true WHERE code = $1',
+export async function update_user(code: string, options: Optional<User>) {
+	const keys = Object.keys(options)
+	if (keys.length === 0)
+		return
+
+	const changes = keys.map((key: keyof User) => {
+		if (typeof options[key] === 'boolean')
+			return `${key} = ${options[key] ? 'true' : 'false'}`
+
+		return `${key} = ${options[key]}`
+	})
+
+	console.log(changes.join(' '))
+
+	const response = pool.query(
+		`UPDATE users SET ${changes.join(' ')} WHERE code = $1`,
 		[ code ]
 	)
 
 	return response
 }
 
-update_gbrsigned('testtesttest').then(console.log)
+export async function delete_user(code: string) {
+	const response = pool.query(
+		'DELETE FROM users WHERE code = $1',
+		[ code ]
+	)
+
+	return response
+}
+
+update_user('fAR4SCGaMB', { gbrsigned: false }).then(console.log)
