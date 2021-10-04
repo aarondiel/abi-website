9e4b0fc2490ae01279610e945c59cab186e14ac2
aarondiel <aaron.diel@t-online.de>
2021-10-04 15:01:22 +0200

    added queries for adding type and resolution field to gallery_images

--- backend/src/models/gallery_images.ts
+++ backend/src/models/gallery_images.ts
@@ -5,7 +5,11 @@ import './users'
 export interface GalleryImage {
 	_id: Types.ObjectId,
 	image: Types.ObjectId,
+	thumbnail300: Types.ObjectId,
+	thumbnail600: Types.ObjectId,
 	submitted_by: Types.ObjectId
+	resolution?: number[]
+	type?: string
 }
 
 const gallery_schema = new Schema<GalleryImage, Model<GalleryImage>, GalleryImage>({
@@ -15,10 +19,32 @@ const gallery_schema = new Schema<GalleryImage, Model<GalleryImage>, GalleryImag
 		required: true
 	},
 
+	thumbnail300: {
+		type: Schema.Types.ObjectId,
+		ref: 'fs.files',
+		required: true
+	},
+
+	thumbnail600: {
+		type: Schema.Types.ObjectId,
+		ref: 'fs.files',
+		required: true
+	},
+
 	submitted_by: {
 		type: Schema.Types.ObjectId,
 		ref: 'users',
 		required: true
+	},
+
+	resolution: {
+		type: [ Number ],
+		required: false
+	},
+
+	type: {
+		type: String,
+		required: false
 	}
 }, { versionKey: false, timestamps: true })
 
--- backend/src/query.ts
+++ backend/src/query.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 import config from './config'
 import gallery from './models/gallery_images'
 import { inspect } from 'util'
+import ffmpeg from 'fluent-ffmpeg'
 
 mongoose.set('runValidators', true)
 
@@ -15,15 +16,111 @@ const mongodb_connect = new Promise<void>((res, rej) => {
 	)
 })
 
+async function gallery_resolution() {
+	const images = await gallery.find()
+
+	if (images === null)
+		return
+
+	const bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db)
+
+	const downloads = images.map(gallery_image => {
+		return new Promise((res, rej) => {
+			const download_stream = bucket.openDownloadStream(gallery_image.image)
+
+			ffmpeg(download_stream).ffprobe(async (err, data) => {
+				if (err)
+					rej(err)
+
+				const resolution = [
+					data.streams[0]?.width,
+					data.streams[0]?.height,
+				]
+
+				if (
+					resolution[0] === undefined ||
+					resolution[1] === undefined
+				)
+					rej('type conversion on resolution failed')
+
+				let format = ''
+				switch (data.format.format_name) {
+					case 'jpeg_pipe':
+						format = 'image/jpeg'
+						break
+
+					default:
+						rej(`unkown type: ${data.format.format_name}`)
+				}
+
+				res(await gallery.updateOne({
+					_id: gallery_image._id },
+					{ $set: {
+						resolution: resolution,
+						type: format
+					}
+				}))
+			})
+		})
+	})
+
+	return await Promise.all(downloads)
+}
+
+async function gallery_thumbnails() {
+	const images = await gallery.find()
+		.limit(1)
+		.populate('image')
+
+	if (images === null)
+		return
+
+	const bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db)
+
+	const downloads = images.map(gallery_image => {
+		if (
+			gallery_image?.thumbnail300 !== undefined &&
+			gallery_image?.thumbnail600 !== undefined
+		)
+			return
+
+		return new Promise<void>((res, rej) => {
+			// @ts-ignore
+			const download_stream = bucket.openDownloadStream(gallery_image.image._id)
+
+			ffmpeg(download_stream)
+				.on('error', rej)
+				.on('end', res)
+
+				.output('./output300.jpg')
+				.complexFilter('scale=300:-1')
+
+				.output('./output600.jpg')
+				.complexFilter('scale=600:-1')
+				.run()
+			})
+	})
+
+	console.log(await Promise.all(downloads))
+
+}
+
 async function main() {
 	await mongodb_connect
 	let query
 
 	switch (process.argv[2]) {
 		case 'gallery_info':
-			console.log(process.argv[3])
 			query = await gallery.findOne({ image: process.argv[3] })
 				.populate('submitted_by')
+			break
+
+		case 'gallery_thumbnails':
+			query = await gallery_thumbnails()
+			break
+
+		case 'gallery_resolution':
+			query = await gallery_resolution()
 			break
 	}
 
@@ -32,4 +129,4 @@ async function main() {
 
 main()
 	.then(() => process.exit(0))
-	.catch(console.log)
+	.catch(console.error)
