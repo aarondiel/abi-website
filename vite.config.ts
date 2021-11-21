11822a353d89f3026a99181fe02bcf26d645297c
aarondiel <aaron.diel@t-online.de>
2021-11-21 21:22:05 +0100

    custom server port

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import mix from 'vite-plugin-mix'
 import { resolve } from 'path'
+import { port } from './config'
 
 export default defineConfig({
+	server: {
+		port: port
+	},
+
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, '.')
 		}
 	},
+
 	plugins: [
 		vue(),
 		mix({ handler: 'backend.ts' })
